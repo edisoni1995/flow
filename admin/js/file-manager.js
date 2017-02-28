b202653e05b9c0c0f8d028cfacc07cdf3ee7c54b
edisoni1995 <edisonbiba91@gmail.com>
2017-02-28 16:34:20 +0100

    Actions added big changes

--- admin/js/file-manager.js
+++ admin/js/file-manager.js
@@ -11,14 +11,14 @@ $(document).ready(function(){
 	// move a file to a folder
 	$('.move-file').click(function(){
 		var name = $('.table input:checked').closest('.file').find('.file-name').text();
-		
+		$('.cancel').removeClass('hidden');
 		$('.move-to').text('Move here').removeClass('hidden');
 	});
 
 	//copy a file to a folder
 	$('.copy-file').click(function(){
 		var name = $('.table input:checked').closest('.file').find('.file-name').text();
-		
+		$('.cancel').removeClass('hidden');
 		$('.move-to').text('Copy here').removeClass('hidden');
 	});
 
@@ -35,22 +35,63 @@ $(document).ready(function(){
 
 	$('#selectAll').click(function(){
 		if($(this).prop('checked')== true){
-			$('.table tbody input[type="checkbox"').prop('checked', true);
+			$('.table tbody tr.file input[type="checkbox"').prop('checked', true);
 			$('.rename-file').attr("disabled", true);
 			$('.copy-link').attr("disabled", true);
+			$('.move-file').attr("disabled", false);
+			$('.copy-file').attr("disabled", false);
+			$('.delete-file').attr("disabled", false);
 		}else{
-			$('.table tbody input[type="checkbox"').prop('checked', false);
-			$('.rename-file').attr("disabled", false);
-			$('.copy-link').attr("disabled", false);
+			$('.table tbody tr.file input[type="checkbox"').prop('checked', false);
+			cancel();
+
 
 		}
 		
 	});
 
-	//on checkbox change hide move to and copy to buttons
+	//cancle move to or copy to action
+	$('.cancel').click(function(){
+		cancel();
+	});
 
+	//on checkbox change hide move to and copy to buttons
+	$('.table tbody input[type="checkbox"]').change(function(){
+		var checked = $('.table tbody input:checkbox:checked');
+		if(checked.length==0){
+			cancel();
+		}else if(checked.length == 1){
+			enableActions();
+			$('#selectAll').prop('checked',false);
+		}else if(checked.length >=2){
+			$('.rename-file').attr("disabled", true);
+			$('.copy-link').attr("disabled", true);
+			$('.copy-link').attr("disabled", true);
+		}
+	});
+
+	//copy selected file link
+	$('.copy-link').click(function() {
+		var link = $('.table input:checked').closest('.file').find('.file-name a').attr('href');
+		var aux = document.createElement("input");
+		aux.setAttribute("value", link);
+		document.body.appendChild(aux);
+		aux.select();
+		document.execCommand("copy");
+	});
+
+	$('.move-to').click(function(){
+		var type = $(this).text();
+		if(type == 'Copy here'){
+			copyFile();
+		}else{
+			moveFile();
+		}
+	});
 	
 });
+
+
 
 
 function createFolder(el){
@@ -59,7 +100,7 @@ function createFolder(el){
 		$('#inputFolder').css("border-color" , "red");
 		return;
 	}
-	var row = '<tr class="folder"><td><fieldset class="form-group"><input type="checkbox" id="checkbox1"><label for="checkbox1"></label></fieldset></td><td></td><td class="folder-name"><i class="fa fa-folder" aria-hidden="true"></i>'+name+'<button type="button" class="btn btn-info move-to hidden">Move here</button></td><td>-</td><td>-</td><td>-</td><td>-</td></tr>';
+	var row = '<tr class="folder"><td></td><td></td><td class="folder-name"><i class="fa fa-folder" aria-hidden="true"></i>'+name+'<button type="button" class="btn btn-info move-to hidden">Move here</button></td><td>-</td><td>-</td><td>-</td><td>-</td></tr>';
 	$('.table tbody').prepend(row);
 	$("#inputFolder").val('');
 	$('#create-folder').modal('hide');
@@ -81,3 +122,31 @@ function deleteFile(el){
 	$('#delete-file').modal('hide');
 }
 
+function cancel(){
+	$('.table tbody tr.file input[type="checkbox"').prop('checked', false);
+	$('#selectAll').prop('checked',false);
+	$('.copy-link').attr('disabled',true);
+	$('.rename-file').attr('disabled',true);
+	$('.delete-file').attr('disabled',true);
+	$('.copy-file').attr('disabled',true);
+	$('.move-file').attr('disabled',true);
+	$('.move-to').addClass('hidden');
+	$('.cancel').addClass('hidden');
+}
+
+function enableActions(){
+	$('.copy-link').attr('disabled',false);
+	$('.copy-file').attr('disabled',false);
+	$('.move-file').attr('disabled',false);
+	$('.delete-file').attr('disabled',false);
+	$('.rename-file').attr('disabled',false);
+}
+
+function copyFile(){
+	console.log('kot');
+}
+
+function moveFile(){
+	$('.table input:checked').closest('.file').remove();
+	cancel();
+}
